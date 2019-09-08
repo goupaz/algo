d7fac724d8be87526e21cb0769363f52a79a096f
Mirali Ahmadli <miraliahmadli@gmail.com>
2019-09-08 19:18:11 +0400

    Simple solution for Single Number problem

--- advanced/codes/python3/leetcode-136-miraliahmadli.py
+++ advanced/codes/python3/leetcode-136-miraliahmadli.py
@@ -8,5 +8,5 @@ class Solution:
     # x^x = 0
     # x^0 = x
     # Order does not matter in XOR operation
-    #Since XOR of element by itself is zero, every element that appears twice will cancel each out and the remaining one wil be our answer
+    # Since 'XOR'ing element with itself is zero, every element that appears twice will cancel each out and the remaining one wil be our answer
     # Time Complexity = O(n), Space Complexity = O(1)
